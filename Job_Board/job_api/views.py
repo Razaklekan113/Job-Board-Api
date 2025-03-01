5a19c86ef6492afe762294f857f28465115a5df0
Razaklekan113 <razaklekan11@gmail.com>
2025-03-01 13:41:13 +0100

    adjusted the user registration

--- Job_Board/job_api/views.py
+++ Job_Board/job_api/views.py
@@ -22,11 +22,10 @@ class UserRegistrationView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            user = serializer.save()
-            token = get_tokens_for_user(user)
-            return Response({"token": token, "msg": "Registration Successful"}, status=status.HTTP_201_CREATED)
+            serializer.save()
+            return Response({"msg": "Registration Successful"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    
 class UserLoginView(APIView):
     renderer_classes = [UserRenderer]
     permission_classes = [permissions.AllowAny]
